8619833ee3a11a9988bd3aeb28e4f572252c5e99
Chris Lanoue <chris@nautilytics.com>
2020-01-24 15:11:06 -0500

    add legend for point

--- react/src/components/App/index.js
+++ react/src/components/App/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Visualization from "./Visualization";
 import {makeStyles} from "@material-ui/core";
+import {MARKER_RADIUS} from "../../constant";
 
 const useStyles = makeStyles(theme => ({
     App: {
         textAlign: 'center'
+    },
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: '275px',
+        marginTop: '-20px',
+        justifyContent: 'start'
+    },
+    point: {
+        width: MARKER_RADIUS * 2,
+        height: MARKER_RADIUS * 2,
+        borderRadius: '50%',
+        backgroundColor: 'steelblue',
+        opacity: .25,
+        marginRight: '10px'
     }
 }));
 
@@ -14,6 +31,10 @@ function App() {
     return (
         <div className={classes.App}>
             <Visualization/>
+            <div className={classes.wrapper}>
+                <div className={classes.point}/>
+                <p>Customer interaction point</p>
+            </div>
         </div>
     );
 }
